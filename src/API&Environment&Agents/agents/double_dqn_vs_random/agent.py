f15a9098dfbd13601c70f549985cd012c8218c17
print-Aleksandar <aleksandartodoroski@zohomail.eu>
2026-08-25 23:39:30 +0200

    back to old version but trying faster 6

--- src/API&Environment&Agents/agents/double_dqn_vs_random/agent.py
+++ src/API&Environment&Agents/agents/double_dqn_vs_random/agent.py
@@ -12,7 +12,7 @@ class Agent:
 
     def __init__(self, num_planes, rows, columns, num_actions, device="cpu",
                  lr=1e-4, gamma=0.99, buffer_capacity=100_000,
-                 batch_size=1024, target_sync_every=1000):
+                 batch_size=512, target_sync_every=1000):
 
         self.device = torch.device(device)
         self.num_actions = num_actions
@@ -22,7 +22,7 @@ class Agent:
 
         raw_policy_net = Network(num_planes, rows, columns, num_actions)
         raw_target_net = Network(num_planes, rows, columns, num_actions)
-        
+
         self.policy_net = raw_policy_net.to(self.device)
         self.target_net = raw_target_net.to(self.device)
 
