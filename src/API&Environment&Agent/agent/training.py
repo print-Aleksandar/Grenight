f3f61b4aca37c95625c33cd7188529d517fddb5d
print-Aleksandar <aleksandartodoroski@zohomail.eu>
2026-09-11 18:40:50 +0200

    dd

--- src/API&Environment&Agent/agent/training.py
+++ src/API&Environment&Agent/agent/training.py
@@ -302,4 +302,4 @@ def train_agent(is_self_play: bool,
         save_checkpoint(agent, episode, agent_step, is_double_net)
         print("Done.")
 
-train_agent(True, True, True, True, False, False, True)
+train_agent(True, True, True, True, False, False, False)
